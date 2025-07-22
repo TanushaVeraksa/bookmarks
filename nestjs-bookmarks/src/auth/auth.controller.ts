9903446a754d10845f618662957611cabe62cd0c
Tanusha161 <54898478+TanushaVeraksa@users.noreply.github.com>
2025-07-22 17:55:06 +0300

    Feature/auth structure (#2)
    * feat: add function signup signin
    
    * feat: add docker-compose for postgresSQL
    
    * feat: implement DB scheme
    
    * feat: add scheme db and implement dto

--- nestjs-bookmarks/src/auth/auth.controller.ts
+++ nestjs-bookmarks/src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    console.log({ dto });
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
 }
--- nestjs-bookmarks/src/auth/auth.service.ts
+++ nestjs-bookmarks/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
-@Injectable({})
-export class AuthService {}
+@Injectable()
+export class AuthService {
+  constructor(private prismaService: PrismaService) {}
+  signin(dto: AuthDto) {
+    return { msg: 'Sign in' };
+  }
+
+  signup() {
+    return { msg: 'Sign up' };
+  }
+}
