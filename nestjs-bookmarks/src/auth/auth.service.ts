dc6d04c4b098301ffbcd0ff4a247ef22400f0205
Tanusha Veraksa <veraksa161@gmail.com>
2025-07-23 14:05:47 +0300

    ref: changed return type

--- nestjs-bookmarks/src/auth/auth.service.ts
+++ nestjs-bookmarks/src/auth/auth.service.ts
@@ -55,15 +55,22 @@ export class AuthService {
     return this.signToken(user.id, user.email);
   }
 
-  private signToken(userId: number, email: string): Promise<string> {
+  private async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
     const payload = {
       sub: userId,
       email,
     };
 
-    return this.jwtService.signAsync(payload, {
+    const token = await this.jwtService.signAsync(payload, {
       expiresIn: '15m',
       secret: this.configService.get('JWT_SECRET'),
     });
+
+    return {
+      access_token: token,
+    };
   }
 }
