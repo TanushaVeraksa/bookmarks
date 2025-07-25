baf237d4ae58695c0921dbc88c096ed3b46e7877
Tanusha Veraksa <veraksa161@gmail.com>
2025-07-25 17:50:07 +0300

    feat: implement test for user endpoint me

--- nestjs-bookmarks/test/app.e2e-spec.ts
+++ nestjs-bookmarks/test/app.e2e-spec.ts
@@ -4,7 +4,6 @@ import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AuthDto } from '../src/auth/dto';
-import passport from 'passport';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -94,18 +93,29 @@ describe('App e2e', () => {
           .spec()
           .post('/auth/signin')
           .withBody(dto)
-          .expectStatus(200);
+          .expectStatus(200)
+          .stores('userAt', 'access_token ');
       });
     });
   });
   describe('User', () => {
-    describe('Get me', () => {});
+    describe('Get me', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200);
+      });
+    });
     describe('Edit user', () => {});
   });
   describe('Bookmarks', () => {
     describe('Create bookmark', () => {});
-    describe('Get bookmark', () => {});
-    describe('Edit bookmark', () => {});
-    describe('Delete bookmark', () => {});
+    describe('Get bookmark by id', () => {});
+    describe('Edit bookmark by id', () => {});
+    describe('Delete bookmark by id', () => {});
   });
 });
